89a1db50d01f08e7bd3323395bc451b81137c107
Ogbodo Uchenna (Osinachi) <ogbodouchenna1425@gmail.com>
2022-02-12 03:08:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/header/Nav';
+import About from './components/main/about/About';
+import Contact from './components/main/contact/Contact';
+import Faq from './components/main/faq/Faq';
+import Menu from './components/main/menu/Menu';
+import Order from './components/main/order/Order';
+import Services from './components/main/services/Services';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Header/>
+      <About/>
+      <Services/>
+      <Menu/>
+      <Faq/>
+      <Contact/>
+      <Order/>
+      <Footer/>
+  
     </div>
   );
 }
